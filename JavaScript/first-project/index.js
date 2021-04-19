ecbb79417c055fe94657a963db1a259a5b26b68b
TualsiJagan49 <jaganmeruga777@msitprogram.net>
2021-04-19 12:53:39 +0530

    Different methods to print out string messages to console in JS.

---
+++ JavaScript/first-project/index.js
@@ -0,0 +1,19 @@
+// This file is for practising the topics in video - 6 of 
+// Beginner's Series to: JavaScript.
+
+// This file has various commands to print out a message to console.
+
+// Method - 1
+console.log('Hello World')
+
+// Method - 2
+console.log("Hello World")
+
+// Method - 3 Using Substitution
+const greetingForHello = "Hello"
+console.log("%s World", greetingForHello)
+
+// Method - 4 String Templating
+const greetingForHi = "Hi,"
+const nameToGreet = "TulasiJagan"
+console.log(`${greetingForHi} ${nameToGreet}`)
