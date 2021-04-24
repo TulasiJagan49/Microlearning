1a8c150f6965a83abd305b03e8bc19747043b8ec
TulasiJagan49 <jaganmeruga777@msitprogram.net>
2021-04-24 16:15:43 +0530

    Math Operators and parsing Number methods

---
+++ JavaScript/first-project/basicmath.js
@@ -0,0 +1,44 @@
+let num = 100;
+
+// Math operators +, -, *, /
+
+console.log(num + 10)
+console.log(num - 109)
+console.log(num * 0.10)
+console.log(num / 110)
+
+// Additional Operators %, ++, --
+console.log(num % 1500)
+console.log(++num)
+console.log(num)
+console.log(--num)
+console.log(num)
+console.log(num++)
+console.log(num)
+console.log(num--)
+console.log(num)
+
+// Math Object
+console.log(Math.PI)
+console.log(Math.sqrt(100))
+
+// parseInt() and parseFloat
+let intNum = 10
+let floNum = 1.5
+
+console.log(parseInt('10'));
+console.log(parseInt(intNum));
+console.log(parseInt('ABC'));
+console.log(parseInt('0xF'));
+console.log(parseInt('1.15'));
+console.log(parseInt('1 + 1'));
+console.log(parseInt('${1 + 1}'));
+
+console.log(parseFloat('1.00'));
+console.log((parseFloat('1.00')).toFixed(2))
+console.log(parseFloat(floNum));
+console.log(parseFloat('ABC'));
+
+
+console.log(floNum.toString());
+console.log(intNum.toString())
